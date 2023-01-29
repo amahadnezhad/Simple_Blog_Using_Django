539c4cc31b5abb05635f26bd916b97827ecd5da0
amahadnezhad <amahadnezhad@gmail.com>
2023-01-29 19:36:27 +0330

    Add author Field to blog.models & register the Model

--- blog/models.py
+++ blog/models.py
@@ -9,7 +9,11 @@ class Post(models.Model):
 
     title = models.CharField(max_length=100)
     text = models.TextField()
-
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
     status = models.CharField(choices=STATUS_CHOICES, max_length=3)
+
+    def __str__(self):
+        return self.title
+
