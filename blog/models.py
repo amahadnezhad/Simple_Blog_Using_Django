114723bf59c163ad45ed041fe11b293de866da3b
amahadnezhad <amahadnezhad@gmail.com>
2023-01-29 19:25:02 +0330

    Add Alter Fields To Blog Model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('pub', 'Published'),
+        ('drf', 'Draft'),
+    )
+
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+    status = models.CharField(choices=STATUS_CHOICES, max_length=3)
