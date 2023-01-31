14bc592a3ba3aa53d27ef5676b659bc0acb2f01c
amahadnezhad <amahadnezhad@gmail.com>
2023-01-31 17:36:05 +0330

    Add post_list_view To The blog App

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Post
+
+
+def post_list_view(request):
+    posts_list = Post.objects.all()
+    return render(request, 'blog/posts_list.html', {'posts_list': posts_list})
