6ef91d599a09010c3520056d68e323adbd99bd5c
amahadnezhad <amahadnezhad@gmail.com>
2023-02-01 16:50:47 +0330

    Add Filter To post_list_view To Show Published Posts (& Tests)

--- blog/tests.py
+++ blog/tests.py
@@ -6,13 +6,20 @@ from .models import Post
 
 
 class BlogPostTest(TestCase):
-    def setUp(self):
-        self.user = User.objects.create(username='user1')
-        self.post1 = Post.objects.create(
+    @classmethod
+    def setUpTestData(cls):
+        user = User.objects.create(username='user1')
+        cls.post1 = Post.objects.create(
             title='Post1',
             text='This is the description of Post 1',
-            status=Post.STATUS_CHOICES[0],
-            author=self.user,
+            status=Post.STATUS_CHOICES[0][0],  # published
+            author=user,
+        )
+        cls.post2 = Post.objects.create(
+            title='Post2',
+            text='This is the description of Post 2',
+            status=Post.STATUS_CHOICES[1][0],  # draft
+            author=user,
         )
 
     def test_post_list_url(self):
@@ -43,3 +50,8 @@ class BlogPostTest(TestCase):
     def test_status_404_if_post_id_not_exist(self):
         response = self.client.get(reverse('post_detail', args=[999]))
         self.assertEqual(response.status_code, 404)
+
+    def test_draft_post_not_show_in_posts_list(self):  # TDD
+        response = self.client.get(reverse('posts_list'))
+        self.assertContains(response, self.post1.title)
+        self.assertNotContains(response, self.post2.title)
