cb7321b146e04bc7a3975b5034ff28e993afde93
amahadnezhad <amahadnezhad@gmail.com>
2023-02-01 17:10:13 +0330

    Create post_create.html & it's URL

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import post_list_view, post_detail_view
+from .views import post_list_view, post_detail_view, post_create_view
 
 urlpatterns = [
     path('', post_list_view, name='posts_list'),
     path('<int:pk>', post_detail_view, name='post_detail'),
+    path('create/', post_create_view,name='post_create'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -11,3 +11,7 @@ def post_list_view(request):
 def post_detail_view(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def post_create_view(request):
+    return render(request, 'blog/post_create.html')
