505f0717e1e67bc47bdae0c6141049b579b5289b
amahadnezhad <amahadnezhad@gmail.com>
2023-02-03 16:07:55 +0330

    Refactor post_list_view To ClassBasedView(PostListView)

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from .views import post_list_view, post_detail_view, post_create_view, post_edit_view, post_delete_view
+from .views import PostListView, post_detail_view, post_create_view, post_edit_view, post_delete_view
 
 urlpatterns = [
-    path('', post_list_view, name='posts_list'),
+    path('', PostListView.as_view(), name='posts_list'),
     path('<int:pk>', post_detail_view, name='post_detail'),
     path('create/', post_create_view, name='post_create'),
     path('<int:pk>/edit/', post_edit_view, name='post_edit'),
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.models import User
+from django.views import generic
 
 from .models import Post
 from .forms import NewPostForm
 
 
-def post_list_view(request):
-    posts_list = Post.objects.filter(status='pub').order_by('-datetime_modified')
-    return render(request, 'blog/posts_list.html', {'posts_list': posts_list})
+class PostListView(generic.ListView):
+    template_name = 'blog/posts_list.html'
+    context_object_name = 'posts_list'
+
+    def get_queryset(self):
+        return Post.objects.filter(status='pub').order_by('-datetime_modified')
 
 
 def post_detail_view(request, pk):
