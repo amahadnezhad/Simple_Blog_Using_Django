218496404de31cc2b0f3c893db62f42055a2bb34
amahadnezhad <amahadnezhad@gmail.com>
2023-02-03 16:14:35 +0330

    Refactor post_detail_view To ClassBasedView(PostDetailView)

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from .views import PostListView, post_detail_view, post_create_view, post_edit_view, post_delete_view
+from .views import PostListView, PostDetailView, post_create_view, post_edit_view, post_delete_view
 
 urlpatterns = [
     path('', PostListView.as_view(), name='posts_list'),
-    path('<int:pk>', post_detail_view, name='post_detail'),
+    path('<int:pk>', PostDetailView.as_view(), name='post_detail'),
     path('create/', post_create_view, name='post_create'),
     path('<int:pk>/edit/', post_edit_view, name='post_edit'),
     path('<int:pk>/delete/', post_delete_view, name='post_delete'),
