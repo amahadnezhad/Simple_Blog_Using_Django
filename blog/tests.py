4deaeeef9ae5ca48b34bb1df8e382fd8dc0f3494
amahadnezhad <amahadnezhad@gmail.com>
2023-02-03 15:46:02 +0330

    Add Tests To Blog App

--- blog/tests.py
+++ blog/tests.py
@@ -8,19 +8,27 @@ from .models import Post
 class BlogPostTest(TestCase):
     @classmethod
     def setUpTestData(cls):
-        user = User.objects.create(username='user1')
+        cls.user = User.objects.create(username='user1')
         cls.post1 = Post.objects.create(
             title='Post1',
             text='This is the description of Post 1',
             status=Post.STATUS_CHOICES[0][0],  # published
-            author=user,
+            author=cls.user,
         )
         cls.post2 = Post.objects.create(
             title='Post2',
             text='This is the description of Post 2',
             status=Post.STATUS_CHOICES[1][0],  # draft
-            author=user,
+            author=cls.user,
         )
+
+    def test_post_model_str(self):
+        post = self.post1
+        self.assertEqual(str(post), post.title)
+
+    def test_post_title(self):
+        self.assertEqual(self.post1.title, 'Post1')
+        self.assertEqual(self.post1.text, 'This is the description of Post 1')
 
     def test_post_list_url(self):
         response = self.client.get('/blog/')
@@ -55,3 +63,19 @@ class BlogPostTest(TestCase):
         response = self.client.get(reverse('posts_list'))
         self.assertContains(response, self.post1.title)
         self.assertNotContains(response, self.post2.title)
+
+    def test_post_create_view(self):
+        response = self.client.post(reverse('post_create'), {
+            'title': 'Some Title',
+            'text': 'This is some text!',
+            'status': 'pub',
+            'author': self.user.id,
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Post.objects.last().title, 'Some Title')
+        self.assertEqual(Post.objects.last().text, 'This is some text!')
+
+
+    def test_post_delete_view(self):
+        response = self.client.post(reverse('post_delete', args=[self.post2.id]), )
+        self.assertEqual(response.status_code, 302)
